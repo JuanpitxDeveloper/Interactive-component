7ee2ea0e69281431ac397960f8a1f64f331fa75c
JuanpitxDeveloper <juanchojm0161@gmail.com>
2024-01-26 10:43:32 -0400

    Inicio de proyecto

---
+++ interactive-component/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className=' w-[100vw] h-[100vh] bg-black'>
+    <h1 className='text-white'>Hola</h1>
+    </div>
+       
+  
+  )
+}
+
+export default App
